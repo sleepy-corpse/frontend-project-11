9f250bc527ab49a44bbce5ba1de07d908012c43c
Rodion Svinuhov <rodion2014@gmail.com>
2022-10-04 15:41:18 +0300

    Upgrade page rendering and error handling

--- src/src/locales/ru.js
+++ src/src/locales/ru.js
@@ -4,7 +4,16 @@ export default {
       errors: {
         invalid: 'Ссылка должна быть валидным URL',
         duplicate: 'RSS уже существует',
+        parsing: 'Ресурс должен содержать валидный RSS',
+        network: 'Ошибка сети',
+      },
+      success: {
+        msg: 'RSS поток успешно загружен',
+      },
+      containerNames: {
+        posts: 'Посты',
+        feeds: 'Фиды',
       }
-    }
-  }
-}
+    },
+  },
+};
--- src/src/parse-rss.js
+++ src/src/parse-rss.js
@@ -1,17 +1,19 @@
 export default (RSSraw) => {
   const parser = new DOMParser();
-  const rss = parser.parseFromString(RSSraw, 'text/xml');
-  const feed = {
-    title: rss.querySelector('title').textContent,
-    description: rss.querySelector('description').textContent,
+  try {
+    const rss = parser.parseFromString(RSSraw, 'text/xml');
+    const feed = {
+      title: rss.querySelector('title').textContent,
+      description: rss.querySelector('description').textContent,
+    };
+    const items = rss.querySelectorAll('item');
+    const posts = Array.from(items).map((item) => ({
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent,
+    }));
+    return { feed, posts };
+  } catch (error) {
+    throw new Error('parsing');
   }
-  const items = rss.querySelectorAll('item');
-  const posts = Array.from(items).map((item) => {
-   return {
-    title: item.querySelector('title').textContent,
-    description: item.querySelector('description').textContent,
-    link: item.querySelector('link').textContent,
-   } 
-  });
-  return { feed, posts };
-}
+};
